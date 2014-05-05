ec1d41a17d2ce54a124d94c181544ad5fd3c0010
Keri Clowes <kerilyn.clowes@gmail.com>
2014-05-05 09:31:41 -0600

    filter through json data to pull out the needed info

--- image_fetcher.rb
+++ image_fetcher.rb
@@ -7,15 +7,29 @@ class ImageFetcher
   end
 
   def count_children
+    @data["data"]["children"].count
   end
 
   def first_child
+    @data["data"]["children"][0]["data"]
   end
 
   def all_images
+    image_array = []
+    @data["data"]["children"].each do |hash|
+      image_array << hash["data"]["url"]
+    end
+    image_array
   end
 
   # fetch only the images that begin with `http://i.imgur.com`
   def only_imgur_images
+    imgur_array = []
+    @data["data"]["children"].each do |hash|
+      if hash["data"]["url"] =~ /http:\/\/i.imgur.com/
+        imgur_array << hash["data"]["url"]
+      end
+    end
+    imgur_array
   end
 end
